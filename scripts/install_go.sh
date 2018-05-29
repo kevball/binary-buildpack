2e1c86c2e2dc71c2fa74e28ed4b5d5608737fb18
Kevin Igarashi-Ball <kevin_ball1@homedepot.com>
2018-05-29 08:42:15 -0400

    Update install_go.sh

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -11,15 +11,18 @@ if [ ! -f $GoInstallDir/go/bin/go ]; then
   URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_MD5:0:8}.tar.gz
 
   echo "-----> Download go ${GO_VERSION}"
-  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
+  curl -vL --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
+  echo "-----> Finished the download of go"
 
   DOWNLOAD_MD5=$(md5sum /tmp/go.tar.gz | cut -d ' ' -f 1)
+  echo "MD5sum: ${DOWNLOAD_MD5}"
 
   if [[ $DOWNLOAD_MD5 != $GO_MD5 ]]; then
     echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $GO_MD5"
     exit 1
   fi
 
+  echo "------> Untaring go"
   tar xzf /tmp/go.tar.gz -C $GoInstallDir
   rm /tmp/go.tar.gz
 fi
